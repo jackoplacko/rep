33e57d132a4f46f7035bc71b9fbb29f5d76caf82
happyalgos <happyalgos@gmail.com>
2016-10-27 10:33:40 +0200

    training

---
+++ fft.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <ios>
+
+#define PI 3.1415926535897932384626
+#define BASE 2
+#define BASEPOW 100
+#define BASEFMT "%02d"
+#define MAX_RAW_LENGTH 1000001
+#define MAX_LENGTH 2000000
+
+using namespace std;
+
+struct Complex {
+      double R, I;
+};
+
+char raw1[MAX_RAW_LENGTH];
+char raw2[MAX_RAW_LENGTH];
+
+double number1[MAX_LENGTH];
+double number2[MAX_LENGTH];
+double number3[MAX_LENGTH];
+
+Complex fft1[MAX_LENGTH];
+Complex fft2[MAX_LENGTH];
+Complex coef[MAX_LENGTH];
+
+void recursiveFFT(Complex *data, Complex *out, int length, int step, int sign) {
+      if (length == 2) {
+          out[0].R = data[0].R + data[step].R;
+          out[0].I = data[0].I + data[step].I;
+          out[1].R = data[0].R - data[step].R;
+          out[1].I = data[0].I - data[step].I;
+          return;
+      }
+
+      Complex *half1 = out;
+      Complex *half2 = out + length / 2;
+      recursiveFFT(data, half1, length / 2, step * 2, sign);
+      recursiveFFT(data + step, half2, length / 2, step * 2, sign);
+
+      double tmpR, tmpI;
+      for (int i = 0; i*2 < length; i++) {
+              double omegaR = cos(2.0 * PI * (double)i / (double)length);
+              double omegaI = sin(2.0 * PI * (double)i / (double)length);
+
+              tmpR = omegaR * half2[i].R - sign * omegaI * half2[i].I;
+              tmpI = omegaR * half2[i].I + sign * omegaI * half2[i].R;
+
+          half2[i].R = half1[i].R - tmpR;
+          half2[i].I = half1[i].I - tmpI;
+          half1[i].R = half1[i].R + tmpR;
+          half1[i].I = half1[i].I + tmpI;
+      }
+}
+
+void FFT(double *data, Complex *out, int length, int nfft) {
+      int i;
+      for (i = 0; i < length; i++) {
+              coef[i].R = data[i];
+              coef[i].I = 0;
+      }
+      for (; i < nfft; i++) {
+              coef[i].R = 0;
+              coef[i].I = 0;
+      }
+      recursiveFFT(coef, out, nfft, 1, 1);
+}
+
+void inverseFFT(Complex *data, double *out, int nfft, int length) {
+      memset(coef, 0, sizeof(Complex) * nfft);
+      recursiveFFT(data, coef, nfft, 1, -1);
+
+      for (int i = 0; i < length; i++) {
+              out[i] = floor(coef[i].R / (double)nfft + 0.5);
+      }
+}
+
+void convolution(Complex *a, Complex *b, Complex *out, long nfft) {
+      double tmpR, tmpI;
+      for (int i = 0; i < nfft; i++) {
+              tmpR = a[i].R * b[i].R - a[i].I * b[i].I;
+              tmpI = a[i].R * b[i].I + a[i].I * b[i].R;
+              out[i].R = tmpR;
+              out[i].I = tmpI;
+      }
+}
+
+int parseBigInt(char *str, double *out, int length) {
+      int pos = 0;
+      for (int i = length - 1; i >= 0; i -= BASE) {
+              double x = str[i] - '0';
+              int offset = 10;
+
+              for (int j = 1; j < BASE && i - j >= 0; j++) {
+                      x += (str[i - j] - '0') * offset;
+                      offset *= 10;
+              }
+              out[pos++] = x;
+      }
+      return pos;
+}
+
+void printBigInt(double *num, int length) {
+      bool zeros = false;
+      for (int i = length; i >= 0; i--) {
+              if (zeros) {
+                      printf(BASEFMT, (int)num[i]);
+              } else {
+                      if (num[i] != 0) {
+                              zeros = true;
+                              printf("%d", (int)num[i]);
+                      }
+              }
+      }
+      if (!zeros) {
+              printf("0");
+      }
+      printf("\n");
+}
+
+void recombine(double *number, int length) {
+    for (int i = 0; i < length; i++) {
+        number[i+1] += (int)number[i] / BASEPOW;
+        number[i] = (int)number[i] % BASEPOW;
+    }
+}
+
+void bigMul() {
+      scanf("%s %s", raw1, raw2);
+
+      if (raw1[0] == '0' || raw2[0] == '0') {
+              printf("0\n");
+              return;
+      }
+
+      int minus = false;
+      char *rawPtr1 = raw1;
+      char *rawPtr2 = raw2;
+      if (rawPtr1[0] == '-') {
+              rawPtr1++;
+              minus = !minus;
+      }
+      if (rawPtr2[0] == '-') {
+              rawPtr2++;
+              minus = !minus;
+      }
+
+      int rawLen1 = strlen(rawPtr1);
+      int rawLen2 = strlen(rawPtr2);
+
+      if (rawLen1 + rawLen2 < 18) {
+              printf("%lld\n", atoll(raw1) * atoll(raw2));
+              return;
+      }
+
+      int length1 = parseBigInt(rawPtr1, number1, rawLen1);
+      int length2 = parseBigInt(rawPtr2, number2, rawLen2);
+      int length3 = length1 + length2 - 1;
+
+      memset(number3, 0, sizeof(double) * (length3 + 2));
+
+      int nfft = 2;
+      while (nfft < length1 + length2) {
+              nfft *= 2;
+      }
+
+      FFT(number1, fft1, length1, nfft);
+      FFT(number2, fft2, length2, nfft);
+      convolution(fft1, fft2, fft1, nfft);
+      inverseFFT(fft1, number3, nfft, length3);
+      recombine(number3, length3);
+
+      if (minus) {
+              printf("-");
+      }
+      printBigInt(number3, length3);
+}
+
+int main() {
+      ios_base::sync_with_stdio(false);
+      int count;
+      scanf("%d", &count);
+
+      for (int i = 0; i < count; i++) {
+              bigMul();
+      }
+}
